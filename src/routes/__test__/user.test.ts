ac3e40e3ce362add47e82387e7c78e8f0216552d
FacuBar <efacubar@gmail.com>
2021-12-17 12:17:46 -0300

    refactoring tests

--- src/routes/__test__/user.test.ts
+++ src/routes/__test__/user.test.ts
@@ -3,143 +3,147 @@ import { app } from '../../index';
 import { User } from '../../models/user';
 import { LoginCookie } from '../../test/setup';
 
-/**
- * Tests for /api/register
- */
-it('should return 400 if registering with an already in use email', async () => {
-  await request(app)
-    .post('/api/register')
-    .send({
+describe('register route', () => {
+  test('registeren with already registered email', async () => {
+    await User.build({
       name: 'Oscaar Isaac',
       email: 'Oscar@Isaac.com',
       password: 'password',
-      passwordConfirmation: 'password',
-    })
-    .expect(201);
+    }).save();
 
-  await request(app)
-    .post('/api/register')
-    .send({
-      name: 'Oscaar Isaac',
-      email: 'Oscar@Isaac.com',
-      password: 'password',
-      passwordConfirmation: 'password',
-    })
-    .expect(400);
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Oscaar Isaac',
+        email: 'Oscar@Isaac.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      })
+      .expect(400);
+  });
+
+  test('successful registration', async () => {
+    const response = await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Oscaar Isaac',
+        email: 'Oscar@Isaac.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      })
+      .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+  });
+
+  test('invalid requests', async () => {
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Oscaar Isaac',
+        email: 'Oscar@Isaac.com',
+        password: 'somepassword',
+        passwordConfirmation: 'someotherpassword',
+      })
+      .expect(400);
+
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: '',
+        email: 'Oscar@Isaac.com',
+        password: 'somepassword',
+        passwordConfirmation: 'somepassword',
+      })
+      .expect(400);
+
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Oscaar Isaac',
+        email: 'Oscarnotanemail',
+        password: 'somepassword',
+        passwordConfirmation: 'somepassword',
+      })
+      .expect(400);
+
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Oscaar Isaac',
+        email: 'oscar@isaac.com',
+        password: '',
+      })
+      .expect(400);
+  });
 });
 
-it('should set a cookie after successfull login', async () => {
-  const response = await request(app)
-    .post('/api/register')
-    .send({
-      name: 'Oscaar Isaac',
-      email: 'Oscar@Isaac.com',
-      password: 'password',
-      passwordConfirmation: 'password',
-    })
-    .expect(201);
-
-  expect(response.get('Set-Cookie')).toBeDefined();
-});
-
-it('should return 400 on bad request', async () => {
-  await request(app)
-    .post('/api/register')
-    .send({
-      name: 'Oscaar Isaac',
-      email: 'Oscar@Isaac.com',
-      password: 'somepassword',
-      passwordConfirmation: 'someotherpassword',
-    })
-    .expect(400);
-
-  await request(app)
-    .post('/api/register')
-    .send({
-      name: '',
-      email: 'Oscar@Isaac.com',
-      password: 'somepassword',
-      passwordConfirmation: 'somepassword',
-    })
-    .expect(400);
-
-  await request(app)
-    .post('/api/register')
-    .send({
-      name: 'Oscaar Isaac',
-      email: 'Oscarnotanemail',
-      password: 'somepassword',
-      passwordConfirmation: 'somepassword',
-    })
-    .expect(400);
-
-  await request(app)
-    .post('/api/register')
-    .send({
-      name: 'Oscaar Isaac',
-      email: 'oscar@isaac.com',
-      password: '',
-    })
-    .expect(400);
-});
-
-/**
- * Tests for /api/login
- */
-it('should return cookie if given valid credentials', async () => {
-  await User.build({
-    name: 'oscar isaac',
-    email: 'oscar@isaac.com',
-    password: 'password',
-  }).save();
-
-  const response = await request(app)
-    .post('/api/login')
-    .send({
+describe('login route', () => {
+  test('successful login', async () => {
+    await User.build({
+      name: 'oscar isaac',
       email: 'oscar@isaac.com',
       password: 'password',
-    })
-    .expect(200);
+    }).save();
 
-  expect(response.get('Set-Cookie')).toBeDefined();
-});
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'oscar@isaac.com',
+        password: 'password',
+      })
+      .expect(200);
 
-it('should return 400 if not given valid credentials', async () => {
-  await User.build({
-    name: 'user',
-    email: 'email@email.com',
-    password: 'password',
-  }).save();
+    expect(response.get('Set-Cookie')).toBeDefined();
+  });
 
-  await request(app)
-    .post('/api/login')
-    .send({
-      email: 'nottheemail@amil.com',
-      password: 'password',
-    })
-    .expect(400);
+  test('invalid request', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        password: 'password',
+      })
+      .expect(400);
 
-  await request(app)
-    .post('/api/login')
-    .send({
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'invalidemail',
+        password: 'password',
+      })
+      .expect(400);
+
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'password',
+      })
+      .expect(400);
+  });
+
+  test('invalid credentials', async () => {
+    await User.build({
+      name: 'user',
       email: 'email@email.com',
-      password: 'notthepassword',
-    })
-    .expect(400);
-
-  await request(app)
-    .post('/api/login')
-    .send({
       password: 'password',
-    })
-    .expect(400);
+    }).save();
 
-  await request(app)
-    .post('/api/login')
-    .send({
-      password: 'password',
-    })
-    .expect(400);
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'nottheemail@amil.com',
+        password: 'password',
+      })
+      .expect(400);
+
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'email@email.com',
+        password: 'notthepassword',
+      })
+      .expect(400);
+  });
 });
 
 /**
